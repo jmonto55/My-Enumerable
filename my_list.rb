739d39c998af03c4700a38eb543832e6baf9754d
ashmalzahra <ashmal.zahraa@gmail.com>
2023-04-07 00:40:36 +0500

    Add file to test

--- my_list.rb
+++ my_list.rb
@@ -1,5 +1,8 @@
+require_relative './my_enumerable'
+
 class MyList
   include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
--- test.rb
+++ test.rb
@@ -1,18 +1,18 @@
 require_relative './my_list'
 # Create our list
-list = MyList.new(1, 2, 3, 4)
+puts list = MyList.new(1, 2, 3, 4)
 # <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
-list.all? { |e| e < 5 }
+puts list.all? { |e| e < 5 }
 # true
-list.all? { |e| e > 5 }
+puts list.all? { |e| e > 5 }
 # false
 
 # Test #any?
-list.any? { |e| e == 2 }
+puts list.any? { |e| e == 2 }
 # true
-list.any? { |e| e == 5 }
+puts list.any? { |e| e == 5 }
 # false
 
 # Test #filter
